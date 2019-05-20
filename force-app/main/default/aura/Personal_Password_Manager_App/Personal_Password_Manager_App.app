7e7d14d75551fc5ad7bf1cc047c6212010ce1260
abhishek-kumar-sharma <mailtoabhisharma96@gmail.com>
2019-05-20 16:13:19 +0530

    Upto username field

--- force-app/main/default/aura/Personal_Password_Manager_App/Personal_Password_Manager_App.app
+++ force-app/main/default/aura/Personal_Password_Manager_App/Personal_Password_Manager_App.app
@@ -1,3 +1,3 @@
-<aura:application>
+<aura:application extends="force:slds">
     <ECSV:login_To_Password_Manager />
 </aura:application>
--- force-app/main/default/lwc/login_To_Password_Manager/login_To_Password_Manager.js
+++ force-app/main/default/lwc/login_To_Password_Manager/login_To_Password_Manager.js
@@ -1,3 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Login_To_Password_Manager extends LightningElement {}
+export default class Login_To_Password_Manager extends LightningElement {
+    /**
+     * Created by Abhishek
+     */
+    @track user = {
+        'userId': undefined,
+    };
+}
