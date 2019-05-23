85e5fe5c249fe296d2db4cc02135bc65bf9e8498
abhishek-kumar-sharma <mailtoabhisharma96@gmail.com>
2019-05-23 20:38:29 +0530

    testre

--- .sfdx/typings/lwc/apex/Login_To_Password_Manager_Controller.d.ts
+++ .sfdx/typings/lwc/apex/Login_To_Password_Manager_Controller.d.ts
@@ -1,3 +1,3 @@
 declare module "@salesforce/apex/Login_To_Password_Manager_Controller.validateUser_Apex" {
-  export default function validateUser_Apex(): Promise<any>;
+  export default function validateUser_Apex(param: {username: any, password: any}): Promise<any>;
 }
