3297fe4e70fa809c8ced7b66ab698410efc73c1d
abhishek-kumar-sharma <mailtoabhisharma96@gmail.com>
2019-05-25 03:19:14 +0530

    logout implemented

--- force-app/main/default/lwc/login_To_Password_Manager/login_To_Password_Manager.js
+++ force-app/main/default/lwc/login_To_Password_Manager/login_To_Password_Manager.js
@@ -3,17 +3,20 @@ import { LightningElement, track } from "lwc";
 import loginData from "@salesforce/apex/Login_To_Password_Manager_Controller.validateUser_Apex";
 export default class Login_To_Password_Manager extends LightningElement {
   /**
+   * Variable section to hold all the condition and intermediate value
    * Created by Abhishek
    */
   @track user = {
     username: undefined,
     password: undefined
   };
-  @track hasPageError = 'slds-hide';
+  @track isLogin = false;
+  @track hasPageError = "slds-hide";
   @track errorMessage = null;
 
   /**
-   * Handle login button
+   * Handle login button action
+   *  Method to check the user record and create session if record exist
    */
   handleLogin() {
     try {
@@ -24,24 +27,39 @@ export default class Login_To_Password_Manager extends LightningElement {
         password: this.user.password
       })
         .then(value => {
-            this.hasPageError = 'slds-hide';
-            this.errorMessage = null;
+          this.hasPageError = "slds-hide";
+          this.errorMessage = null;
+          this.isLogin = true;
+          sessionStorage.setItem("loginId", value);
           console.log("value -->", value);
+          console.log("test", sessionStorage.getItem("loginId"));
         })
         .catch(error => {
-            this.hasPageError = 'slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error sdls-m-bottom_small';
-            this.errorMessage = error.body.message;
+          this.hasPageError =
+            "slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error sdls-m-bottom_small";
+          this.errorMessage = error.body.message;
           console.log("Error ", error.body.message);
         });
     } catch (exception) {
-        this.hasPageError = 'slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error sdls-m-bottom_small';
-        this.errorMessage = exception.message;
+      this.hasPageError =
+        "slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error sdls-m-bottom_small";
+      this.errorMessage = exception.message;
       console.error(
         "Exception occuured while login. \n Message ::" + exception.message
       );
     }
   }
 
+  /**
+   * Method to handle the logout button action
+   * This method will invalidate the active session of user
+   */
+  handleLogout() {
+    this.hasPageError = "slds-hide";
+    this.errorMessage = null;
+    this.isLogin = false;
+    sessionStorage.clear();
+  }
   /**
    * Handler method to set the input value , name must be same as UI
    */
