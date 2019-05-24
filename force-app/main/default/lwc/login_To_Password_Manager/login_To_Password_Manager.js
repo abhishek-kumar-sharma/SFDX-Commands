20185bb0003e055de919f7fa4d10591df1ecd5a5
abhishek-kumar-sharma <mailtoabhisharma96@gmail.com>
2019-05-24 20:24:55 +0530

    test hello

--- force-app/main/default/lwc/login_To_Password_Manager/login_To_Password_Manager.js
+++ force-app/main/default/lwc/login_To_Password_Manager/login_To_Password_Manager.js
@@ -1,31 +1,49 @@
 /* eslint no-console:["error",{allow:["warn","error","log"]}] */
-import { LightningElement, track } from 'lwc';
-
+import { LightningElement, track } from "lwc";
+import loginData from "@salesforce/apex/Login_To_Password_Manager_Controller.validateUser_Apex";
 export default class Login_To_Password_Manager extends LightningElement {
-    /**
-     * Created by Abhishek
-     */
-    @track user = {
-        'username': undefined,
-        'password': undefined
-    };
+  /**
+   * Created by Abhishek
+   */
+  @track user = {
+    username: undefined,
+    password: undefined
+  };
 
-    /**
-     * Handle login button 
-     */
-    handleLogin() {
-        console.log('user data ==>', this.user.username);
-        console.log('user data ==>', this.user.password);
+  /**
+   * Handle login button
+   */
+  handleLogin() {
+    try {
+      console.log("user data ==>", this.user.username);
+      console.log("user data ==>", this.user.password);
+      loginData({
+        username: this.user.username,
+        password: this.user.password
+      })
+        .then(value => {
+          console.log("value -->", value);
+        })
+        .catch(error => {
+          console.log("Error ", error);
+        });
+    } catch (exception) {
+      console.error(
+        "Exception occuured while login. \n Message ::" + exception.message
+      );
     }
+  }
 
-    /**
-     * Handler method to set the input value , name must be same as UI
-     */
-    setInputValue(event) {
-        if (event.target.value !== null && event.target.value !== undefined && event.target.value.trim() !== '') {
-            this.user[event.target.name] = event.target.value;
-        }
-
+  /**
+   * Handler method to set the input value , name must be same as UI
+   */
+  setInputValue(event) {
+    if (
+      event.target.value !== null &&
+      event.target.value !== undefined &&
+      event.target.value.trim() !== ""
+    ) {
+      this.user[event.target.name] = event.target.value;
     }
-
+  }
 }
