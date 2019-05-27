caa1e169e7220f6430183fc4d5467d3a52d7f104
abhishek-kumar-sharma <mailtoabhisharma96@gmail.com>
2019-05-27 11:19:02 +0530

    start of the day(27-05-2019)

--- force-app/main/default/lwc/login_To_Password_Manager/login_To_Password_Manager.js
+++ force-app/main/default/lwc/login_To_Password_Manager/login_To_Password_Manager.js
@@ -2,7 +2,8 @@
 import { LightningElement, track } from "lwc";
 import loginData from "@salesforce/apex/Login_To_Password_Manager_Controller.validateUser_Apex";
 export default class Login_To_Password_Manager extends LightningElement {
-  /**
+  
+    /**
    * Variable section to hold all the condition and intermediate value
    * Created by Abhishek
    */
@@ -20,8 +21,6 @@ export default class Login_To_Password_Manager extends LightningElement {
    */
   handleLogin() {
     try {
-      console.log("user data ==>", this.user.username);
-      console.log("user data ==>", this.user.password);
       loginData({
         username: this.user.username,
         password: this.user.password
@@ -31,8 +30,6 @@ export default class Login_To_Password_Manager extends LightningElement {
           this.errorMessage = null;
           this.isLogin = true;
           sessionStorage.setItem("loginId", value);
-          console.log("value -->", value);
-          console.log("test", sessionStorage.getItem("loginId"));
         })
         .catch(error => {
           this.hasPageError =
@@ -58,12 +55,14 @@ export default class Login_To_Password_Manager extends LightningElement {
     this.hasPageError = "slds-hide";
     this.errorMessage = null;
     this.isLogin = false;
+    this.user = {};
     sessionStorage.clear();
   }
+
   /**
    * Handler method to set the input value , name must be same as UI
    */
-  setInputValue(event) {
+    setInputValue(event) {
     if (
       event.target.value !== null &&
       event.target.value !== undefined &&
@@ -72,4 +71,14 @@ export default class Login_To_Password_Manager extends LightningElement {
       this.user[event.target.name] = event.target.value;
     }
   }
+
+/**
+ * Method to get all the data from apex after login
+ * Created by Abhishek Kumar Sharma
+ * Created Date MAY 25,2019
+ */
+  getDataFromApex(){    
+    
+}
+
 }
